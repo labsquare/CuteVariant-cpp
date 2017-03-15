9f59328132a9414b4bf5eff1b50d25909c85da73
dridk <sacha@labsquare.org>
2017-03-15 19:50:56 +0100

    clean python script

--- import_vcf.py
+++ import_vcf.py
@@ -1,15 +1,30 @@
 import sys 
 import vcf 
 import re
+import os 
 from peewee import * 
 
-# ================= MODEL =============================
+filename = os.path.abspath(sys.argv[1])
+
+dirpath  = os.path.dirname(os.path.realpath(filename))
+basename = os.path.splitext(os.path.basename(filename))[0]
+dbname   = os.path.join(dirpath,basename+".db")
+
 try:
-	os.remove("variant.db")
+	os.remove(dbname)
 except : 
 	pass
 
-db = SqliteDatabase('variant.db')
+db = SqliteDatabase(dbname)
+
+
+# ================= MODEL =============================
+class Projet(Model):
+	dbName   = CharField()
+	filename = CharField()
+	checksum = CharField()
+
+
 class Variant(Model):
 	chrom = CharField()
 	pos   = IntegerField()
@@ -49,70 +64,60 @@ db.create_tables([Variant, Field,Sample], safe = True)
 
 # ================= IMPORT =============================
 
-
-filename = sys.argv[1]
-print("import", filename)
-
-SPECIAL_FIELDS = ("ANN","LOF","NMD")
-
-reader = vcf.Reader(open(filename,'r'))
-
-#  PARSE HEADERS AND CREATE ANNOTATION TABLE ACCORDING FIELDS NUMBER
-for info in reader.infos:
-	if info in SPECIAL_FIELDS:
-		desc = reader.infos[info].desc
-		bloc = re.findall(r"'(.+)'", desc)
-		fields = [i.strip() for i in bloc[0].split("|")]
-		for i in range(len(fields)):
-			key = info +"_" + str(i+1)
-			f = Field(colname = key, description = fields[i])
+def import_variant(filename):
+	print("Creating", dbname) 
+	SPECIAL_FIELDS = ("ANN","LOF","NMD")
+	reader = vcf.Reader(open(filename,'r'))
+	#  PARSE HEADERS AND CREATE ANNOTATION TABLE ACCORDING FIELDS NUMBER
+	for info in reader.infos:
+		if info in SPECIAL_FIELDS:
+			desc = reader.infos[info].desc
+			bloc = re.findall(r"'(.+)'", desc)
+			fields = [i.strip() for i in bloc[0].split("|")]
+			for i in range(len(fields)):
+				key = info +"_" + str(i+1)
+				f = Field(colname = key, description = fields[i])
+				f.save()
+				# Append new columns no annotation 
+				new_col = CharField(null=True)
+				new_col.add_to_class(Annotation, key)
+			# Create annotation table
+		else:
+			key = info 
+			f   = Field(colname = key, description = reader.infos[info].desc)
 			f.save()
-			# Append new columns no annotation 
 			new_col = CharField(null=True)
 			new_col.add_to_class(Annotation, key)
-		# Create annotation table
-	else:
-		key = info 
-		f   = Field(colname = key, description = reader.infos[info].desc)
-		f.save()
-		new_col = CharField(null=True)
-		new_col.add_to_class(Annotation, key)
+	Annotation.create_table()
 
-	
+	# IMPORT VARIANT AND ANNOTATION DATA
+	with db.transaction():
+		for record in reader:
+			for alt_i in range(len(record.ALT)):
+				variant     = Variant.create(chrom = record.CHROM, pos = record.POS, ref = record.REF, alt = record.ALT[alt_i] )
+				ann         = Annotation()
+				ann.variant = variant
 
-Annotation.create_table()
+				for k in record.INFO.keys():
+					if k in SPECIAL_FIELDS:
+						raw_ann = record.INFO[k]
+						i = 1
+						for value in raw_ann[0].split("|"):
+							key = k+"_"+str(i)
+							setattr(ann,key,value)
+							i+=1
 
-
-# IMPORT VARIANT AND ANNOTATION DATA
-with db.transaction():
-	for record in reader:
-		for alt_i in range(len(record.ALT)):
-			variant     = Variant.create(chrom = record.CHROM, pos = record.POS, ref = record.REF, alt = record.ALT[alt_i] )
-			ann         = Annotation()
-			ann.variant = variant
-
-			for k in record.INFO.keys():
-				if k in SPECIAL_FIELDS:
-					raw_ann = record.INFO[k]
-					i = 1
-					for value in raw_ann[0].split("|"):
-						key = k+"_"+str(i)
-						setattr(ann,key,value)
-						i+=1
-
-				else:
-					if isinstance(record.INFO[k], list):
-						if len(record.ALT) == len(record.INFO[k]):
-							setattr(ann,k, record.INFO[k][alt_i])
-						else:
-							setattr(ann,k, record.INFO[k][0])
 					else:
-						setattr(ann,k, record.INFO[k])
+						if isinstance(record.INFO[k], list):
+							if len(record.ALT) == len(record.INFO[k]):
+								setattr(ann,k, record.INFO[k][alt_i])
+							else:
+								setattr(ann,k, record.INFO[k][0])
+						else:
+							setattr(ann,k, record.INFO[k])
+				ann.save()
 
+# ================= IMPORT =============================
 
-			ann.save()
+import_variant(filename)
 
-
-
-
-## Remove empty columns 
